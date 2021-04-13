6b42e46f57d684ff3023ffb25fdac7a3afcd6bc0
sakai-tatsunori <kinakotarou.booou@gmail.com>
2021-04-13 08:43:52 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = Book.new()
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+ def create
+   @book = Book.new(book_params)
+   if @book.save
+    redirect_to book_path(@book.id), notice: "successfully"
+   else
+    @books = Book.all
+    render action: :index
+   end
+
+ end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+ 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id), notice: "successfully"
+    else
+      render action: :edit
+    end
+  end
+ 
+  def destroy
+    @book = Book.find(params[:id])
+  if @book.destroy
+    redirect_to books_path, notice: "successfully"
+  else
+    render action: :index
+  end
+  end
+ 
+private
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
+end
